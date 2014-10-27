9fabbfab5526e272c8c4601b9a6f6203cabc9840
Mahbub Rony <rony.cse36@gmail.com>
2014-10-27 16:56:35 +0600

    Gem Only For Active Admin and Rails 3.0.x

--- app/controllers/ckeditor/pictures_controller.rb
+++ app/controllers/ckeditor/pictures_controller.rb
@@ -1,8 +1,8 @@
 class Ckeditor::PicturesController < Ckeditor::BaseController
 
   def index
-    if Ckeditor.file_manager_partition_users and respond_to?(:current_user)
-      @pictures = Ckeditor.image_model.where(:user_id => current_user).order("id DESC")
+    if Ckeditor.file_manager_partition_users and respond_to?(:current_admin_user)
+      @pictures = Ckeditor.image_model.where(:admin_user_id => current_admin_user).order("id DESC")
     else
       @pictures = Ckeditor.image_model.order("id DESC")
     end
@@ -10,8 +10,8 @@ class Ckeditor::PicturesController < Ckeditor::BaseController
   end
 
   def create
-    if Ckeditor.file_manager_partition_users and respond_to?(:current_user)
-      if Ckeditor.image_model.where(:user_id => current_user).count <= Ckeditor.swf_image_file_upload_limit
+    if Ckeditor.file_manager_partition_users and respond_to?(:current_admin_user)
+      if Ckeditor.image_model.where(:admin_user_id => current_admin_user).count <= Ckeditor.swf_image_file_upload_limit
         @picture = Ckeditor.image_model.new
       end
     else
